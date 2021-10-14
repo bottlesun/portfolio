2a6109d09cf75512b62bdbe48699ea98e1ec9a47
bottmesun <bottlesun3514.gmail.com>
2021-10-14 23:12:41 +0900

    portfolio

---
+++ js/main.js
@@ -0,0 +1,46 @@
+
+/* 글자 마우스 오버 시 변경  */
+function textChange(){
+let contactText = document.getElementById('context');
+contactText.innerText = 'Contact Me!';
+
+}
+
+/* 글자 마우스 아웃 시 변경 */
+function textChange2(){
+    let contactText = document.getElementById('context');
+    contactText.innerText = 'About Me?';
+    
+    }
+
+
+/* 스크롤 이벤트 */
+document.addEventListener('scroll', function() {
+   let currentScrollValue = document.documentElement.scrollTop; //스크롤 위치 구하기
+   let icon = document.getElementById('icons');
+
+   // console.log('지금 스크롤의 위치는? ' + currentScrollValue + 'px'); //스크롤 위치 콘솔에 출력
+    
+    //위치에따른 이벤트 주기
+    if(currentScrollValue <= 100){
+
+        icon.classList.add('offdisplay');
+        icon.classList.remove('ondisplay');
+
+    } else{
+        icon.classList.add('ondisplay');
+        icon.classList.remove('offdisplay');
+    }
+    
+    });
+
+ /* 제일 밑으로 */
+function ClickonBottom() {
+    window.scrollTo(0,document.body.scrollHeight);
+}
+
+/* 제일 위로 */
+function ClickonTop() {
+window.scrollTo(0,document.body.scrollTop);
+}
+
