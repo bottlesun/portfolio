e0509c0eeb768ab3fb78bafa030b53ca33c90dc5
bottlesun <bottlesun3514@gmail.com>
2021-11-23 15:07:41 +0900

    portfolio_2021.11.23

--- js/main.js
+++ js/main.js
@@ -47,22 +47,31 @@ window.scrollTo(0,document.body.scrollTop);
 
 // 탭에 필요한 리스트 아이템 
 const tabList = document.querySelectorAll(".portfolio_bar li a");
+
+
 let currentTab; // 클릭이벤트가 전달된 엘리먼트
 let parentEl; // 연결된 엘리먼트의 부모
 let childEl; // 부모 엘리먼트의 자식 엘리먼트들
 let tabIndex; // 연결된 엘리먼트의 인덱스
 
 let tabSlideAction = (tabEvents)=>{
+
+    for (i=0;i < tabList.length; i++){
+        tabList[i].classList.remove('bar_action')
+    }
+    
 	currentTab = tabEvents.currentTarget;
   	parentEl = currentTab.parentElement;
   	childEl = parentEl.children;
   	tabIndex = Array.from(childEl).indexOf(currentTab);
-    
-    currentTab.classList.add('bar_action');
-    console.log(parentEl)
-   
+
+       
+      currentTab.classList.add('bar_action');
+      console.log(currentTab)
 }
 
 tabList.forEach((items)=>{
   items.addEventListener("click", tabSlideAction)
 })
+
+
