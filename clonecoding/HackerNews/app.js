94aff22c6f5ea4596580c3b461e274f67bec6c34
bottlesun <bottlesin3514@gmail.com>
2022-01-18 18:33:25 +0900

    source_22.01.18_hackernewsAPI

---
+++ clonecoding/HackerNews/app.js
@@ -0,0 +1,88 @@
+const container = document.getElementById('root');
+let ajax = new XMLHttpRequest();
+const content = document.createElement('div');
+const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+
+const store = {
+    currentPage : 1,
+};
+
+function getData(url) {
+    ajax.open('GET', url, false);
+    ajax.send();
+
+    return JSON.parse(ajax.response);
+}
+
+
+function newsFeed() { //목록화면
+    const newsFeed = getData(NEWS_URL);
+    const newsList = [];
+  
+    newsList.push('<ul>');
+    
+    for(let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
+      newsList.push(`
+        <li>
+          <a href="#/show/${newsFeed[i].id}">
+            ${newsFeed[i].title} (${newsFeed[i].comments_count})
+          </a>
+        </li>
+      `);
+    }
+
+    newsList.push('</ul>');
+
+    newsList.push(`
+        <div>
+        <a id="prev" href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전 페이지</a>
+        <a id="naxt" href="#/page/${ store.currentPage < 3 ? store.currentPage + 1 : store.currentPage }">다음 페이지</a>
+        </div>
+    `);
+    container.innerHTML = newsList.join('');
+
+    const prev = document.querySelector('#prev');
+    const naxt = document.querySelector('#naxt');
+
+    if(store.currentPage === 1 ){
+        prev.style.color = '#eaeaea';
+        prev.style.cursor="Default"
+    }
+
+    if(store.currentPage === 3 ){
+        naxt.style.color = '#eaeaea';
+        naxt.style.cursor="Default"
+    }
+}
+
+function newsDefail() { // 내용 화면 
+    const id = location.hash.substring(7);
+    const newsContent = getData(CONTENT_URL.replace(`@id`, id));
+
+    container.innerHTML = `
+    <h1>${newsContent.title}</h1>
+
+    <div>
+    <a href="#/page/${store.currentPage}">목록으로</a>
+    </div>
+    `
+};
+
+
+function router(){
+    const routePath = location.hash;
+
+    if(routePath === ''){ //location.hash 에 #만 들어왔을경우엔 빈 값을 반환
+        newsFeed();
+    } else if(routePath.indexOf('#/page/') >= 0 ){
+        store.currentPage = Number(routePath.substring(7));
+        newsFeed();
+    } else {
+        newsDefail();
+    }
+}
+
+window.addEventListener('hashchange', router);
+
+router();
