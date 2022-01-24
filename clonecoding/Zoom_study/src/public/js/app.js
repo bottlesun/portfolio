e75f8e24adc06129e47604b5dbbe7a1b350e588e
bottlesun <bottlesun3514@gmail.com>
2022-01-24 23:26:40 +0900

    Zoom_WSSever_22.01.24

--- clonecoding/Zoom_study/src/public/js/app.js
+++ clonecoding/Zoom_study/src/public/js/app.js
@@ -1,1 +1,18 @@
-alert('hello');
+const FrontSocket = new WebSocket(`ws://${window.location.host}`);
+
+
+FrontSocket.addEventListener("open" , () => {
+    console.log('Connected to Server ✔')
+});
+
+FrontSocket.addEventListener('message', (message) =>{
+    console.log("New message : " , message.data, message.data);
+});
+
+FrontSocket.addEventListener('close', () => {
+    console.log("DisConected from Sever ❌");
+});
+
+setTimeout(() => {
+    FrontSocket.send("hello from the browser!");
+}, 10000);
--- clonecoding/Zoom_study/src/server.js
+++ clonecoding/Zoom_study/src/server.js
@@ -1,3 +1,5 @@
+import http from "http"; // ws
+import WebSocket from "ws";  // ws
 import express from 'express';
 
 const app = express();
@@ -5,8 +7,24 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use("/public" , express.static(__dirname + "/public"));
-app.get("/" , (req,res) => res.render('home'));
+app.get("/" , (_,res) => res.render('home'));
+app.get("/*" , (_,res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-app.listen(3000,handleListen);
+
+/* ws 구현 */
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection",(socket) =>{
+    console.log("Connected to Browser ✔");
+    //socket 에 있는 기능
+    socket.on("close", ()=>  console.log("DisConected from Browser ❌"));
+    socket.on("message" , message => {
+        console.log(message.toString());
+    })
+    socket.send("hello!");
+});
+
+server.listen(3000,handleListen);
