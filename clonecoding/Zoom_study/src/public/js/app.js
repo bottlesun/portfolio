bf1ed1936ab0885b7b446611d6b4eab2b6a377e9
unknown <bottlesun@gmail.com>
2022-01-31 18:09:24 +0900

    zoom_study_welcome&bye_22.01.31

--- clonecoding/Zoom_study/src/public/js/app.js
+++ clonecoding/Zoom_study/src/public/js/app.js
@@ -3,19 +3,81 @@ const soket = io(); // back-end 와 연결 해줌
 
 const welcome = document.getElementById('welcome');
 const form = welcome.querySelector("form");
+const room = document.getElementById('room');
 
-function backendDone(msg){
-    console.log(`The backend says : ` , msg);
+room.hidden = true;
+
+let roomName;
+
+function addMessage(message){
+    const ul = room.querySelector("ul");
+    const li = document.createElement("li")
+    li.innerText = message;
+    ul.appendChild(li);
+}
+
+function handleMessageSubmit(e){
+    e.preventDefault();
+    const input = room.querySelector('#msg input');
+    const value = input.value;
+    soket.emit('new_message', input.value , roomName , ()=>{
+        addMessage(`You : ${value}`);
+    });
+    input.value = ""
+
+}
+
+function handleNickNameSubmit(e){
+    e.preventDefault();
+    const input = room.querySelector('#name input');
+    soket.emit("nickname" , input.value);
+}
+
+function showRoom(){
+   welcome.hidden = true;
+   room.hidden = false;
+   const h3 = room.querySelector("h3");
+   h3.innerText =  `Room :${roomName}`;
+   const msgForm = room.querySelector('#msg');
+   const nameForm = room.querySelector('#name');
+   msgForm.addEventListener('submit',handleMessageSubmit);
+   nameForm.addEventListener('submit',handleNickNameSubmit);
 }
 
 function handleRoomSubmit(e) {
     e.preventDefault();
     const input = form.querySelector('input');
-    soket.emit("enter_room" , {payload : input.value} ,  backendDone );
+    soket.emit("enter_room" , input.value ,  showRoom );
+    roomName = input.value;
     input.value = "";
 }
 
 form.addEventListener('submit', handleRoomSubmit);
+
+soket.on("welcome" , (nickname)=>{
+    addMessage(`${nickname} 님께서 입장 하셨습니다.`);
+});
+
+soket.on('bye',(nickname)=>{
+    addMessage(`${nickname} 님께서 퇴장 하셨습니다.`);
+})
+
+soket.on('new_message' , addMessage);
+
+soket.on('room_change' , (rooms) => {
+    const roomList = welcome.querySelector('ul');
+
+    if(rooms.length === 0) {
+        roomList.innerHTML = "";
+        return;
+    }
+    rooms.forEach((room) => {
+        const li = document.createElement('li');
+        li.innerText = room;
+        roomList.append(li);
+    })
+});
+
 
 /* WebSocket 
 const messageList = document.querySelector("ul");
--- clonecoding/Zoom_study/src/server.js
+++ clonecoding/Zoom_study/src/server.js
@@ -7,9 +7,9 @@ const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
-app.use("/public" , express.static(__dirname + "/public"));
-app.get("/" , (_,res) => res.render('home'));
-app.get("/*" , (_,res) => res.redirect("/"));
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (_, res) => res.render('home'));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
@@ -25,15 +25,49 @@ function onSocketClose() {
     console.log("DisConected from Browser ❌")
 }
 */
+function publicRooms() {
+  //  const sids = wsServer.sockets.adapter.sids;
+  //  const rooms = wsServer.sockets.adapter.rooms;
+  const {
+    sockets: {
+      adapter: { sids, rooms },
+    },
+  } = wsServer;
+  const publicRooms = [];
+  rooms.forEach((_,key) => {
+    if(sids.get(key) === undefined) {
+      publicRooms.push(key)
+    }
+  });
+  return publicRooms;
+}
 
-
-wsServer.on('connection', socket => {
-  socket.on('enter_room', (roomName, done) =>{
-    console.log(roomName);
-    setTimeout(()=>{
-      done("hello from the backend");
-    },10000)
-  }, 15000);
+wsServer.on('connection', (socket) => {
+  // wsServer.socketsJoin("announcement"); 강제로 방에 넣는 방법
+  socket['nickname'] = `Guest${socket.id}`
+  socket.onAny((event) => {
+    console.log(`Soket Event : ${event}`);
+  });
+  socket.on('enter_room', (roomName, done) => {
+    socket.join(roomName);
+    done();
+    socket.to(roomName).emit("welcome", socket.nickname);
+    wsServer.sockets.emit("room_change" ,publicRooms());
+  });
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach(room =>
+      socket.to(room).emit("bye", socket.nickname)
+    );
+    wsServer.sockets.emit("room_change" ,publicRooms());
+  });
+  socket.on("disconnet" , ()=> {
+    wsServer.sockets.emit("room_change" ,publicRooms());
+  })
+  socket.on("new_message", (msg, room, done) => {
+    socket.to(room).emit("new_message", `${socket.nickname} : ${msg}`);
+    done();
+  });
+  socket.on("nickname", nickname => (socket["nickname"] = nickname));
 });
 
 /* websocket code 
@@ -59,4 +93,4 @@ wss.on("connection",(socket) =>{
 */
 
 
-httpServer.listen(3000,handleListen);
+httpServer.listen(3000, handleListen);
