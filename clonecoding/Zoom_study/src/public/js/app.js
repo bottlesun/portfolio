96db2696a22dadc41fa409bd20ab0cfa45defeb8
unknown <bottlesun@gmail.com>
2022-02-07 15:01:06 +0900

    video start 22.02.07

--- clonecoding/Zoom_study/src/public/js/app.js
+++ clonecoding/Zoom_study/src/public/js/app.js
@@ -1,1 +1,23 @@
 const soket = io(); 
+
+
+const myFace = document.getElementById('myFace');
+const muteBtn = document.getElementById('mute');
+const cameraBtn = document.getElementById('camera');
+
+let myStream; 
+
+async function getMedia(){
+    try{
+        myStream = await navigator.mediaDevices.getUserMedia(
+           {
+               audio : false,
+               video : true,
+           });
+        myFace.srcObject = myStream;
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+getMedia();
--- clonecoding/Zoom_study/src/server.js
+++ clonecoding/Zoom_study/src/server.js
@@ -1,7 +1,6 @@
 import http from "http"; // ws
 // import WebSocket from "ws";  // ws
 import { Server } from 'socket.io';
-import {instrument} from '@socket.io/admin-ui';
 import express from 'express';
 
 const app = express();
@@ -12,13 +11,10 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render('home'));
 app.get("/*", (_, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-
 
 /* ws 구현 */
 const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer);
 
-
-
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
