6ef8f74792c3161614b5d894c98da94bb4974e51
bottlesun <bottlesun3514@gmail.com>
2022-02-10 23:29:40 +0900

    22.02.10 zoom video btn

--- clonecoding/Zoom_study/src/public/js/app.js
+++ clonecoding/Zoom_study/src/public/js/app.js
@@ -6,12 +6,15 @@ const muteBtn = document.getElementById('mute');
 const cameraBtn = document.getElementById('camera');
 
 let myStream; 
+let muted = false;
+let cameraOff = false;
+
 
 async function getMedia(){
     try{
         myStream = await navigator.mediaDevices.getUserMedia(
            {
-               audio : false,
+               audio : true,
                video : true,
            });
         myFace.srcObject = myStream;
@@ -21,3 +24,25 @@ async function getMedia(){
 }
 
 getMedia();
+
+function handleMuteClick(){
+    if(!muted){
+        muteBtn.innerText = "Unmute";
+        muted = true;
+    } else{
+        muteBtn.innerText = "Mute";
+        muted = false; 
+    }
+    
+}
+function handleCameraClick(){
+    if(cameraOff){
+        cameraBtn.innerText = "Turn Camera Off"
+        cameraOff = false;
+    } else {
+        cameraBtn.innerText = "Turn Camera On"
+        cameraOff = true;
+    }
+}
+muteBtn.addEventListener('click' ,handleMuteClick);
+cameraBtn.addEventListener('click',handleCameraClick);
