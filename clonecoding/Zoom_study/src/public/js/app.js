da8911c6bf930bccc880d4654dd8911bbae58edd
unknown <bottlesun@gmail.com>
2022-02-15 00:00:37 +0900

    zoom video audio on off / video label 22.02.15

--- clonecoding/Zoom_study/src/public/js/app.js
+++ clonecoding/Zoom_study/src/public/js/app.js
@@ -4,10 +4,26 @@ const soket = io();
 const myFace = document.getElementById('myFace');
 const muteBtn = document.getElementById('mute');
 const cameraBtn = document.getElementById('camera');
+const camerasSelect = document.getElementById('cameras');
 
 let myStream; 
 let muted = false;
 let cameraOff = false;
+
+async function getCameras(){
+    try{
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter(devices => devices.kind === "videoinput");
+        cameras.forEach(camera => {
+            const option = document.createElement('option');
+            option.value = camera.deviceId;
+            option.innerText = camera.label;
+            camerasSelect.appendChild(option);
+        })
+    } catch(e) {
+        console.log(e)
+    }
+}
 
 
 async function getMedia(){
@@ -18,6 +34,7 @@ async function getMedia(){
                video : true,
            });
         myFace.srcObject = myStream;
+        await getCameras();
     } catch (e) {
         console.log(e);
     }
@@ -26,7 +43,9 @@ async function getMedia(){
 getMedia();0
 
 function handleMuteClick(){
-   console.log(myStream.getAudioTracks())
+   myStream
+        .getAudioTracks()
+        .forEach((track) => (track.enabled = !track.enabled) );
     if(!muted){
         muteBtn.innerText = "Unmute";
         muted = true;
@@ -37,6 +56,9 @@ function handleMuteClick(){
     
 }
 function handleCameraClick(){
+    myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled) );
     if(cameraOff){
         cameraBtn.innerText = "Turn Camera Off"
         cameraOff = false;
