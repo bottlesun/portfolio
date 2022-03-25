de9321f7966adcd3c2c30075be2726ffb00217a2
bottlesun <bottlesun3514@gmail.com>
2022-03-25 17:43:10 +0900

    22.03.25 NewsTimes API Filter & KeyWord & error Code Refactoring

--- 05-NewsTimes/app.js
+++ 05-NewsTimes/app.js
@@ -28,39 +28,82 @@ const openSearchBox = () => {
 
 
 let news = [];
+let ThisMenus = document.querySelectorAll('.menus button')
+let thisValue = ''
+let searchBtn = document.querySelector('.search-button');
+let url = ''
+ThisMenus.forEach(menu => menu.addEventListener('click', (event) => getNewsByTopic(event)));
+
+
+// 각 함수에서 필요한 url을 만든다.
+// api 호출 함수를 부른다.
+const getNews = async () => {
+    try {
+        let header = new Headers({ 'x-api-key': 'tlfAWwOpBrAQQa3g6LQ5f-a_9E3txFZcgrw2oJx1i_0' }); // js 내장 클래스 new Headers({})
+        let response = await fetch(url, { headers: header });
+        let data = await response.json();
+        if (response.status == 200) {
+            if(data.total_hits == 0){
+                throw new Error('검색된 결과값이 없습니다.');
+            }
+            news = data.articles;
+            console.log(news);
+            render();
+        } else {
+            throw new Error(data.message);
+        }
+
+    } catch (error) {
+        console.log('잡힌에러는', error.message);
+        errorRender(error.message);
+    }
+
+}
 
 
 /* api */
 const getLatestNews = async () => {
-    let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=business&page_size=10`); // js 내장 클래스 new URL()
-    let header = new Headers({ 'x-api-key': 'tlfAWwOpBrAQQa3g6LQ5f-a_9E3txFZcgrw2oJx1i_0' }); // js 내장 클래스 new Headers({})
-
-    let response = await fetch(url, { headers: header });
-    let data = await response.json();
-    news = data.articles;
-    console.log(news)
-    render()
+    url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=10`); // js 내장 클래스 new URL()
+    getNews()
 }
-getLatestNews()
+
+const getNewsByTopic = async (event) => {
+    console.log('클릭됨', event.target.textContent);
+    let topic = event.target.textContent.toLowerCase()
+    url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=${topic}&page_size=10`);
+    getNews()
+}
+
+const getNewsByKeyWord = async () => {
+    // 검색키워드 읽어오기
+    // url에 검색 키워드 넣기
+    // 헤더 준비
+    // url 부르기
+    // 데이터 가지고 오기
+    // 데이터 보내기
+
+    let keyWord = document.querySelector('#search-input').value;
+    url = new URL(`https://api.newscatcherapi.com/v2/search?q=${keyWord}&countries=KR&page_size=10`);
+
+    getNews()
+}
 
 function render() {
+    //topic
     let newsHTML = "";
     newsHTML = news.map((newsItem) => {
         return `<div class="row news">
         <div class="col-lg-4">   
-            <img class="news-img-size" src="${
-                newsItem.media || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU"}" alt="">
+            <img class="news-img-size" src="${newsItem.media || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU"}" alt="">
         </div>
         <div class="col-lg-8 news-content">
             <h2>${newsItem.title}</h2>
             <P>
-                ${
-                    newsItem.summary == "null" ||  newsItem.summary == "" ? "내용없음" :  newsItem.summary.length > 200 ? newsItem.summary.substring(0,200) + "..." : newsItem.summary
-                }
+                ${newsItem.summary == "null" || newsItem.summary == "" ? "내용없음" : newsItem.summary.length > 200 ? newsItem.summary.substring(0, 200) + "..." : newsItem.summary
+            }
             </P>
             <div class="source-dey">
-            ${newsItem.rights||"no Source"} * ${
-                moment(newsItem.published_date).fromNow()
+            ${newsItem.rights || "no Source"} * ${moment(newsItem.published_date).fromNow()
             }
             </div>
         </div>
@@ -71,3 +114,18 @@ function render() {
     document.querySelector('.body_wrap').innerHTML = newsHTML;
 
 }
+
+
+searchBtn.addEventListener('click', getNewsByKeyWord);
+
+const errorRender = (message) => {
+    let errorHTML = `
+    <div class="alert alert-danger text-center" role="alert">
+  ${message}
+</div>
+    `;
+    document.querySelector('.body_wrap').innerHTML = errorHTML;
+}
+
+
+getLatestNews()
