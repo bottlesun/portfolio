b8b235d438979aaaad1f0c16e98ae3eacfc47d25
bottlesun <bottlesun3514@gmail.com>
2022-03-29 17:23:53 +0900

    NewsTimes 22.03.29

--- 05-NewsTimes/app.js
+++ 05-NewsTimes/app.js
@@ -22,7 +22,7 @@ const openSearchBox = () => {
 
 let news = [];
 let page = 1;
-let total_pages = 0;
+let total_page = 0;
 let ThisMenus = document.querySelectorAll('.menus button')
 let thisValue = ''
 let searchBtn = document.querySelector('.search-button');
@@ -37,19 +37,21 @@ const getNews = async () => {
         let header = new Headers({ 'x-api-key': 'tW8owYAyg01t9XS_JJ9f8R37c2cEzH8dOcZ0A5zeYOI' }); // js 내장 클래스 new Headers({})
         //tW8owYAyg01t9XS_JJ9f8R37c2cEzH8dOcZ0A5zeYOI ->API mr_k00
         //tlfAWwOpBrAQQa3g6LQ5f-a_9E3txFZcgrw2oJx1i_0 ->API krr1996
+
+        url.searchParams.set('page', page); // &page=
         let response = await fetch(url, { headers: header });
         let data = await response.json();
         if (response.status == 200) {
-            if(data.total_hits == 0){
+            if (data.total_hits == 0) {
                 throw new Error('검색된 결과값이 없습니다.');
             }
-            console.log('데이터' , data)
+            console.log('데이터', data)
             news = data.articles;
-            total_pages = data.total_page;
+            total_page = data.total_pages;
             page = data.page;
             console.log(news);
             render();
-            pagenation();
+            pageNation();
         } else {
             throw new Error(data.message);
         }
@@ -126,23 +128,69 @@ const errorRender = (message) => {
     document.querySelector('.body_wrap').innerHTML = errorHTML;
 }
 
-const pagenation = () => {
-    let pagenationHTML = ``;
-    // total_page
-    // page
-    // page group
-    let pagegroup = Math.ceil(page/5);
-    //last
-    let last = pagegroup * 5;
-    //first
-    let first = last - 4;
-    // first ~ last 페이지 프린트
-    for (let i = first; i <= last ; i++){
-      pagenationHTML += `<li class="page-item"><a class="page-link" href="#">${i}</a></li>`
+const pageNation = () => {
+    let pageNationHTML = ``;
+    let pageGroup = Math.ceil(page / 5);
+    let last = pageGroup * 5;
+    if (last > total_page) {
+      // 마지막 그룹이 5개 이하이면
+      last = total_page;
     }
-    
-    document.querySelector('.pagination').innerHTML = pagenationHTML;
-    
+    let first = last - 4 <= 0 ? 1 : last - 4; // 첫그룹이 5이하이면
+    // first ~ last 페이지 프린트
+
+    //total page 3일 경우 3개의 페이지만 프린트 하는법 last, first 
+    //<< >> 버튼 만들어주기 맨 처음 맨 끝으로 가는 버튼 만들기 
+    // 내가 그룹 1일때, << < 버튼이 없다
+    // 내가 마지막 그룹일떄 > >> 버튼이 없다
+
+
+    pageNationHTML = page == 1 ? "" : 
+    `
+    ${
+        page >= false + 1 ? `<li class="page-item">
+        <a class="page-link" href="#" aria-label="Previous" onClick="moveToPage(${false})">
+          <span aria-hidden="true">&laquo;</span>
+        </a>
+      </li>` : ""
+    }
+    <li class="page-item">
+    <a class="page-link" href="#" aria-label="Previous" onClick="moveToPage(${page-1})">
+      <span aria-hidden="true">&lt;</span>
+    </a>
+  </li>
+  `
+    for (let i = first; i <= last; i++) {
+        pageNationHTML += `
+        <li class="page-item ${page == i ? 'active' : ""}"><a class="page-link " href="javascript:void(0)" onClick="moveToPage(${i})">${i}</a></li>
+        `
+    }
+
+    pageNationHTML += page == total_page ? "" :  `
+    <li class="page-item">
+    <a class="page-link" href="#" aria-label="Next" onClick="moveToPage(${page+1})">
+      <span aria-hidden="true">&gt;</span>
+    </a>
+  </li>
+  ${
+    page <= total_page - 1 ? `<li class="page-item">
+    <a class="page-link" href="#" aria-label="Next" onClick="moveToPage(${total_page})">
+      <span aria-hidden="true">&raquo;</span>
+    </a>
+  </li>` : ""
+}
+`
+
+    document.querySelector('.pagination').innerHTML = pageNationHTML;
+
+}
+
+const moveToPage = (pageNum) => {
+    //1. 이동하고싶은 페이지 알기
+    page = pageNum;
+    console.log(page)
+    //2. 이동하고싶은 페이지를 가지고 API를 재호출
+    getNews()
 }
 
 searchBtn.addEventListener('click', getNewsByKeyWord);
