0d23f721bad4e50b7d7f580141ccede8f6dceca1
bottlesun <bottlesun3514@gmail.com>
2022-04-05 18:12:05 +0900

    weather start 22.04.05

---
+++ 06-Weather/app.js
@@ -0,0 +1,19 @@
+// api key - 89e3fe4c9f2cf5204d70b1b9bddc97fa
+//API 호출 URL - https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+
+/*
+const ApiKey = '89e3fe4c9f2cf5204d70b1b9bddc97fa'
+const url = `https://api.openweathermap.org/data/2.5/weather?q=KOREA&=lang=kr&appid=89e3fe4c9f2cf5204d70b1b9bddc97fa`
+
+const getData = () => {
+    axios.get(url)
+        .then(response => {
+            console.log(response.data)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
+
+getData()
+*/
