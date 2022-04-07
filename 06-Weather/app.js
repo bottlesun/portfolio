9b7cf38d726a359545edd1eff78b03de426a6cd8
bottlesun <bottlesun3514@gmail.com>
2022-04-07 19:53:48 +0900

    weather API 22.04.07

--- 06-Weather/app.js
+++ 06-Weather/app.js
@@ -1,96 +1,105 @@
 // api key - 89e3fe4c9f2cf5204d70b1b9bddc97fa
 //API 호출 URL - https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 const APIkey = '89e3fe4c9f2cf5204d70b1b9bddc97fa'
-let API = `https://api.openweathermap.org/data/2.5/weather?q=seoul,KR&appid=${APIkey}`
-let CityJSON = './city.list.json' 
-let Datas = []
-let City = []
+let API = ""
+let API2 = ""
+let API3 = ""
+let Datas1 , Datas2,  Datas3= []
 
 
-const getData = () => {
+let Arr = [1,2,3]
+const getData = async () => {
 
-    const requestOne = axios.get(API);
-    const requestTwo = axios.get(CityJSON);
-    
-    axios.all([requestOne,requestTwo])
-        .then(
-            axios.spread((...response) => {
-                let data = response[0].data;
-                let CityData = response[1].data
-               // console.log(data)
-               // console.log(CityData)
-                Datas = data;
-                City = CityData;
-                render()
-                CityList()
- 
-            })
-        )
-        .catch(res => {
-            console.log(res)
-        })
+    try {
+        const response = await axios.get(API);
+        const response2 = await axios.get(API2);
+        const response3 = await axios.get(API3);
 
+        let data = response.data
+        let data2 = response2.data
+        let data3 = response3.data
+
+        Datas1 = data;
+        Datas2 = data2;
+        Datas3 = data3;
+
+        render()
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-const render = () => {
-    let createHTML = "";
+const getApi = async () => {
 
-    for(let i = 0; i < Datas.weather.length;i++){
-     let description =  Datas.weather[i].description   
-
-    createHTML += `<article class="weather_item rounded drop-shadow bg-indigo-400">
-    <div class="main_info flex">
-        <div class="weather_icon"><i class="fa-solid "></i></div>
-        <div class="weather_info flex gap-2">
-            <div class="flex centers gap-2"><i class="fa-solid fa-temperature-half"></i> <span>${(Datas.main.feels_like - 273.15).toFixed(1)}℃</span></div>
-            <p>${description}</p>
-            <p>${Datas.name} , ${Datas.sys.country == "KR" ? "한국" : Datas.sys.country }</p>
-        </div>
-    </div>
-    <ul class="sub_info centers flex mx-auto p-5 bg-white">
-        <li><i class="fa-solid fa-temperature-arrow-up"><span>${(Datas.main.temp_max - 273.15).toFixed(1)}℃</span></i></li>
-        <li><i class="fa-solid fa-temperature-arrow-down"><span>${(Datas.main.temp_min - 273.15).toFixed(1)}℃</span></i></li>
-    </ul>
-</article>`
-    }
+        let url = await 'https://api.openweathermap.org/data/2.5/weather?&appid='
+        API  = `${url}${APIkey}&q=seoul`;
+        API2 = `${url}${APIkey}&q=Daegu`;
+        API3 = `${url}${APIkey}&q=Goyang`;
     
+    getData()
+    
+}
+
+
+const render =  () => {
+    
+    let createHTML = "";
+    for(let no = 1 ; no <= Arr.length ; no++){
+        let Datas = ( new Function( 'return ' + `Datas${no}` ) )(); 
+   
+        for (let i = 0; i < Datas.weather.length; i++) {
+            let description = Datas.weather[i].description;
+            createHTML += `<article class="weather_item rounded drop-shadow 
+            ${((Datas.main.feels_like - 273.15).toFixed(1) >= 25) ? "hot" : ((Datas.main.feels_like - 273.15).toFixed(1) <= 5) ? "cool" : 'soso' }">
+        <div class="main_info flex">
+            <div class="weather_icon"><i class="fa-solid "></i></div>
+            <div class="weather_info flex gap-2">
+                <div class="flex centers gap-2"><i class="fa-solid fa-temperature-half"></i> <span>${(Datas.main.feels_like - 273.15).toFixed(1)}℃</span></div>
+                <p>${description}</p>
+                <p>${(Datas.name == 'Seoul') ? "서울" : (Datas.name == 'Daegu') ? "대구" : (Datas.name == 'Goyang-si') ? "고양" : Datas.name } , ${Datas.sys.country == "KR" ? "한국" : Datas.sys.country}</p>
+            </div>
+        </div>
+        <ul class="sub_info centers flex mx-auto p-5 bg-white">
+            <li><i class="fa-solid fa-temperature-arrow-up"><span>${(Datas.main.temp_max - 273.15).toFixed(1)}℃</span></i></li>
+            <li><i class="fa-solid fa-temperature-arrow-down"><span>${(Datas.main.temp_min - 273.15).toFixed(1)}℃</span></i></li>
+        </ul>
+    </article>`
+        }
+    }
+
+
+    
+
     document.querySelector('.weather').innerHTML = createHTML;
     changeWeatherIcon();
 }
 
 
+/* 날씨 아이콘 변경 */
 const changeWeatherIcon = () => {
-    let weather_icon = document.querySelector('.weather_icon i')
-    for(let i = 0; i < Datas.weather.length;i++){
-        let description =  Datas.weather[i].description;
-        if(description == 'clear sky') {
-            weather_icon.classList.add('fa-sun');
-        } else if (description == 'few clouds'){
-            weather_icon.classList.add('fa-cloud-sun');
-        } else if (description == 'broken clouds' || description == 'scattered clouds'){
-            weather_icon.classList.add('fa-cloud');
-        } else if (description == 'rain' || description == 'shower rain'){
-            weather_icon.classList.add('fa-cloud-rain');
-        } else if (description == 'thunderstorm'){
-            weather_icon.classList.add('fa-cloud-bolt');
-        } else if (description == 'snow') {
-            weather_icon.classList.add('fa-snowflake');
-        } else if (description == 'mist'){
-            weather_icon.classList.add('fa-water');
-        }
-    }
-}
-
-const CityList = () => {
-    City.map((i)=>{
-        if(i.country == 'KR'){
-            if(i.name.includes('-si')){
-                API = `https://api.openweathermap.org/data/2.5/weather?q=${i.name},KR&appid=${APIkey}`
-                console.log(API)
+    let weather_icon = document.querySelectorAll('.weather_icon i');
+    weather_icon.forEach(icon => {
+        for (let i = 0; i < Datas1.weather.length; i++) {
+            let description = Datas1.weather[i].description;
+            if (description == 'clear sky') {
+                icon.classList.add('fa-sun');
+            } else if (description == 'few clouds') {
+                icon.classList.add('fa-cloud-sun');
+            } else if (description == 'broken clouds' || description == 'scattered clouds' || description == 'overcast clouds') {
+                icon.classList.add('fa-cloud');
+            } else if (description == 'rain' || description == 'shower rain') {
+                icon.classList.add('fa-cloud-rain');
+            } else if (description == 'thunderstorm') {
+                icon.classList.add('fa-cloud-bolt');
+            } else if (description == 'snow') {
+                icon.classList.add('fa-snowflake');
+            } else if (description == 'mist') {
+                icon.classList.add('fa-water');
             }
         }
     });
+   
 }
 
 
-getData()
+getApi();
