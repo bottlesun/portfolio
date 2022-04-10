b63c1f065f589c57ba07eacd7a0438de59cfc492
bottlesun <bottlesun3514@gmail.com>
2022-04-11 00:38:59 +0900

    weathers API 22.04.11

--- 06-Weather/app.js
+++ 06-Weather/app.js
@@ -1,57 +1,40 @@
 // api key - 89e3fe4c9f2cf5204d70b1b9bddc97fa
 //API 호출 URL - https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 const APIkey = '89e3fe4c9f2cf5204d70b1b9bddc97fa'
-let url=""
-
+let url = ""
 let API = ""
-let API2 = ""
-let API3 = ""
 
-let CityURL = 'cityList-min.json'
-let Datas1, Datas2, Datas3, CityDatas = []
+let Datas1, Arr = []
 
-let button  = document.querySelectorAll('.buttonAction')
+let button = document.querySelectorAll('.buttonAction')
+let closeBtn = document.querySelectorAll('.closeBtn');
 
-let Arr = [1, 2, 3]
+
+
 const getData = async () => {
 
     try {
         const response = await axios.get(API);
-        const response2 = await axios.get(API2);
-        const response3 = await axios.get(API3);
-
-        const JsonResponse = await axios.get(CityURL);
 
         let data = response.data;
-        let data2 = response2.data;
-        let data3 = response3.data;
-        let CityDaTa = JsonResponse.data;
-
-
+        Arr.push(API)
         Datas1 = data;
-        Datas2 = data2;
-        Datas3 = data3;
-        CityDatas = CityDaTa;
-
         render()
-        City()
-
 
     } catch (error) {
         console.log(error)
     }
 }
 
-button.forEach((i)=> {
+button.forEach((i) => {
     i.addEventListener('click', (event) => menuButton(event))
 });
+
 
 const getApi = async () => {
 
     url = await 'https://api.openweathermap.org/data/2.5/weather?&appid='
     API = `${url}${APIkey}&q=seoul`;
-    API2 = `${url}${APIkey}&q=Daegu`;
-    API3 = `${url}${APIkey}&q=Goyang`;
 
     getData()
 
@@ -59,10 +42,12 @@ const getApi = async () => {
 
 
 const render = () => {
-
+    let list = [];
     let createHTML = "";
-    for (let no = 1; no <= Arr.length; no++) {
-        let Datas = (new Function('return ' + `Datas${no}`))();
+    list = Arr;
+
+    for (let no = 1; no <= list.length; no++) {
+        let Datas = (new Function('return ' + `Datas1`))();
 
         for (let i = 0; i < Datas.weather.length; i++) {
             let description = Datas.weather[i].description;
@@ -73,19 +58,18 @@ const render = () => {
             <div class="weather_info flex gap-2">
                 <div class="flex centers gap-2"><i class="fa-solid fa-temperature-half"></i> <span>${(Datas.main.feels_like - 273.15).toFixed(1)}℃</span></div>
                 <p>${description}</p>
-                <p>${(Datas.name == 'Seoul') ? "서울" : (Datas.name == 'Daegu') ? "대구" : (Datas.name == 'Goyang-si') ? "고양" : Datas.name} , ${Datas.sys.country == "KR" ? "한국" : Datas.sys.country}</p>
+                <p class="weatherCity"> ${Datas.name}  , ${Datas.sys.country == "KR" ? "한국" : Datas.sys.country}</p>
             </div>
         </div>
         <ul class="sub_info centers flex mx-auto p-5 bg-white">
             <li><i class="fa-solid fa-temperature-arrow-up"><span>${(Datas.main.temp_max - 273.15).toFixed(1)}℃</span></i></li>
             <li><i class="fa-solid fa-temperature-arrow-down"><span>${(Datas.main.temp_min - 273.15).toFixed(1)}℃</span></i></li>
         </ul>
-    </article>`
+        <div class="closeBtn"><i class="fa-solid fa-x"></i></div>
+    </article>
+    `
         }
     }
-
-
-
 
     document.querySelector('.weather').innerHTML = createHTML;
     changeWeatherIcon();
@@ -102,9 +86,9 @@ const changeWeatherIcon = () => {
                 icon.classList.add('fa-sun');
             } else if (description == 'few clouds') {
                 icon.classList.add('fa-cloud-sun');
-            } else if (description == 'broken clouds' || description == 'scattered clouds' || description == 'overcast clouds') {
+            } else if (description.includes('clouds') || !(description == 'few clouds')) {
                 icon.classList.add('fa-cloud');
-            } else if (description == 'rain' || description == 'shower rain') {
+            } else if (description.includes('rain')) {
                 icon.classList.add('fa-cloud-rain');
             } else if (description == 'thunderstorm') {
                 icon.classList.add('fa-cloud-bolt');
@@ -118,32 +102,35 @@ const changeWeatherIcon = () => {
 
 }
 
-/* 상단 메뉴 버튼 */
-const City = async () => {
-
-   for  await(const index of CityDatas) {
-        if (index.country == 'KR') {
-            if (index.name.includes('-do') && !index.name.includes('-dong')) {
-                console.log(index.name)
-            }
-        }
-    }
-
-}
 
 const menuButton = async (event) => {
     let thisButton = event.target;
     url = await 'https://api.openweathermap.org/data/2.5/weather?&appid='
 
-    if(thisButton.innerText == '경기도' ){
-     API = `${url}${APIkey}&q=Gyeonggi-do`;
-    } else if(thisButton.innerText == '강원도'){
+    if (thisButton.innerText == '경기도') {
+        API = `${url}${APIkey}&q=Gyeonggi-do`;
+    } else if (thisButton.innerText == '강원도') {
         API = `${url}${APIkey}&q=Gangwon-do`;
+    } else if (thisButton.innerText == '충청북도') {
+        API = `${url}${APIkey}&q=Chungcheongbuk-do`;
+    } else if (thisButton.innerText == '충청남도') {
+        API = `${url}${APIkey}&q=Chungcheongnam-do`;
+    } else if (thisButton.innerText == '전라북도') {
+        API = `${url}${APIkey}&q=Jeollabuk-do`;
+    } else if (thisButton.innerText == '전라남도') {
+        API = `${url}${APIkey}&q=Jeollanam-do`;
+    } else if (thisButton.innerText == '경상북도') {
+        API = `${url}${APIkey}&q=Gyeongsangbuk-do`;
+    } else if (thisButton.innerText == '경상남도') {
+        API = `${url}${APIkey}&q=Gyeongsangnam-do`;
+    } else if (thisButton.innerText == '제주도') {
+        API = `${url}${APIkey}&q=Jeju-do`;
+    } else {
+        API = `${url}${APIkey}&q=Seoul`;
     }
 
     getData()
 }
-
 
 
 getApi();
