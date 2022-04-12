c471889d5f3879bf5c4b67de9495ba4cf3fb9a8a
bottlesun <bottlesun3514@gmail.com>
2022-04-13 01:31:56 +0900

    22.04.13 날씨API  페이지네이션 진행중 중복오류 수정 예정

--- 06-Weather/js/app.js
+++ 06-Weather/js/app.js
@@ -6,19 +6,19 @@ let url = "";
 let API = "";
 let Datas, Arr = [];
 let DatasList = [];
-
 let CityArr = [
-    { City: "Gyeonggi-do", name: '경기도', done: false },
-    { City: "Gangwon-do", name: '강원도', done: false },
-    { City: "Chungcheongbuk-do", name: '충청북도', done: false },
-    { City: "Chungcheongnam-do", name: '충청남도', done: false },
-    { City: "Jeollabuk-do", name: '전라북도', done: false },
-    { City: "Jeollanam-do", name: '전라남도', done: false },
-    { City: "Gyeongsangbuk-do", name: '경상북도', done: false },
-    { City: "Gyeongsangnam-do", name: '경상남도', done: false },
-    { City: "Jeju-do", name: '제주도', done: false },
+    { City: "Gyeonggi-do", name: '경기도', done: true },
+    { City: "Gangwon-do", name: '강원도', done: true },
+    { City: "Chungcheongbuk-do", name: '충청북도', done: true },
+    { City: "Chungcheongnam-do", name: '충청남도', done: true },
+    { City: "Jeollabuk-do", name: '전라북도', done: true },
+    { City: "Jeollanam-do", name: '전라남도', done: true },
+    { City: "Gyeongsangbuk-do", name: '경상북도', done: true },
+    { City: "Gyeongsangnam-do", name: '경상남도', done: true },
+    { City: "Jeju-do", name: '제주도', done: true },
     { City: "Seoul", name: '서울', done: false },
 ];
+
 
 let page = 1;
 let totalPage = 0;
@@ -60,34 +60,39 @@ const getApi = async () => {
 
 
 const render = () => {
-    let list = DatasList;
-    let createHTML = "";
-    for (let no = 0; no < list.length; no++) {
+    try {
+        let list = DatasList;
+        let createHTML = "";
+        for (let no = 0; no < list.length; no++) {
 
-        for (let i = 0; i < list[no].weather.length; i++) {
-            let description = list[no].weather[i].description;
-            createHTML += `<article class="weather_item rounded drop-shadow overflow-hidden
-            ${((list[no].main.feels_like - 273.15).toFixed(1) >= 20) ? "hot" : ((list[no].main.feels_like - 273.15).toFixed(1) <= 5) ? "cool" : 'soso'}">
-        <div class="main_info flex" >
-            <div class="weather_icon"><i class="fa-solid "></i></div>
-            <div class="weather_info flex gap-2">
-                <div class="flex centers gap-2"><i class="fa-solid fa-temperature-half"></i> <span>${(list[no].main.feels_like - 273.15).toFixed(1)}℃</span></div>
-                <p>${description}</p>
-                <p class="weatherCity"> ${list[no].name}  , ${list[no].sys.country == "KR" ? "한국" : list[no].sys.country}</p>
+            for (let i = 0; i < list[no].weather.length; i++) {
+                let description = list[no].weather[i].description;
+                createHTML += `<article class="weather_item rounded drop-shadow overflow-hidden
+                ${((list[no].main.feels_like - 273.15).toFixed(1) >= 20) ? "hot" : ((list[no].main.feels_like - 273.15).toFixed(1) <= 5) ? "cool" : 'soso'}">
+            <div class="main_info flex" >
+                <div class="weather_icon"><i class="fa-solid "></i></div>
+                <div class="weather_info flex gap-2">
+                    <div class="flex centers gap-2"><i class="fa-solid fa-temperature-half"></i> <span>${(list[no].main.feels_like - 273.15).toFixed(1)}℃</span></div>
+                    <p>${description}</p>
+                    <p class="weatherCity"> ${list[no].name}  , ${list[no].sys.country == "KR" ? "한국" : list[no].sys.country}</p>
+                </div>
             </div>
-        </div>
-        <ul class="sub_info centers flex mx-auto p-5 bg-white">
-            <li><i class="fa-solid fa-temperature-arrow-up"><span>${(list[no].main.temp_max - 273.15).toFixed(1)}℃</span></i></li>
-            <li><i class="fa-solid fa-temperature-arrow-down"><span>${(list[no].main.temp_min - 273.15).toFixed(1)}℃</span></i></li>
-        </ul>
-        <div class="closeBtn" onclick="Clicks(${list[no].id})"><i class="fa-solid fa-x"></i></div>
-    </article>
-    `
+            <ul class="sub_info centers flex mx-auto p-5 bg-white">
+                <li><i class="fa-solid fa-temperature-arrow-up"><span>${(list[no].main.temp_max - 273.15).toFixed(1)}℃</span></i></li>
+                <li><i class="fa-solid fa-temperature-arrow-down"><span>${(list[no].main.temp_min - 273.15).toFixed(1)}℃</span></i></li>
+            </ul>
+            <div class="closeBtn" onclick="Clicks(${list[no].id})"><i class="fa-solid fa-x"></i></div>
+        </article>
+        `
+            }
         }
-
+        document.querySelector('.weather').innerHTML = createHTML;
+        changeWeatherIcon();
+    } catch (error) {
+        console.log(error.message);
+        errorRender(error.message)
     }
-    document.querySelector('.weather').innerHTML = createHTML;
-    changeWeatherIcon();
+
 }
 
 
@@ -123,41 +128,70 @@ const menuButton = async (event) => {
 
     let CityName = CityArr;
 
-
     let thisButton = event.target;
     url = await 'https://api.openweathermap.org/data/2.5/weather?&appid='
 
     for (let i = 0; i < CityName.length; i++) {
-        if (thisButton.innerText == CityName[i].name) {
-            API = `${url}${APIkey}&q=${CityName[i].City}`;
-            getData();
+        if (CityName[i].done) {
+            if (thisButton.innerText == CityName[i].name) {
+                API = `${url}${APIkey}&q=${CityName[i].City}`;
+                getData();
+                return CityName[i].done = false
+            }
+        } else {
+            console.log('중복')
         }
     }
 }
 
 /* 닫기 */
-const Clicks = async(id) => {
+const Clicks = async (id) => {
     DatasList;
-    for(let i = 0 ; i < DatasList.length ; i++){
-        if(DatasList[i].id == id){
+    for (let i = 0; i < DatasList.length; i++) {
+        if (DatasList[i].id == id) {
             DatasList.splice(i, 1);
             break;
         }
+    }
+
+    for (let i = 0; i < CityArr.length; i++) {
+        CityArr[i].done = true;
     }
     render();
     pagelation();
 }
 
+const errorRender = (error) => {
+    let errorHTML = `
+    <div class="alert  text-center" role="alert">
+  ${error}
+</div>
+    `
+    document.querySelector('section').innerHTML = errorHTML;
+}
+
+
 /* pagelation */
 const pagelation = () => {
     let pageNationHTML = "";
-    totalPage = Math.ceil( DatasList.length / 3);
+    const dataGroup = 3;
+    const dataPage = 10;
+
+    totalPage = Math.ceil(DatasList.length / dataGroup);
+    if (dataGroup < totalPage) {
+        totalPage = dataGroup;
+    }
+
     console.log(totalPage)
-    let last = totalPage * 3;
-    let first = last - 2;
-    console.log(DatasList)
-    for(let i = 1; i <= totalPage; i ++) {
-    pageNationHTML += `
+    let last = totalPage * dataGroup;
+    if (last < totalPage) {
+        last = totalPage
+    }
+
+    let first = last - (dataGroup - 1);
+
+    for (let i = 1; i <= totalPage; i++) {
+        pageNationHTML += `
     <li class="${page == i ? 'active' : ""} bg-indigo-500 shadow-lg py-1 px-4 shadow-indigo-500/50 p-2 py-2 rounded hover:bg-white hover:text-indigo-500" onClick="moveTo(${i})">${i}</li>
     `
     }
@@ -167,8 +201,8 @@ const pagelation = () => {
 
 const moveTo = (pageNum) => {
     page = pageNum;
-
-    
+    console.log(page)
+    getData()
 }
 
 getApi();
