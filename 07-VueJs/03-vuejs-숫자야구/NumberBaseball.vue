9c15dff177507d4a1e38e874077ef68da6ac9404
bottlesun <bottlesun@gmail.com>
2022-05-16 18:40:24 +0900

    vue study 22.05.16

---
+++ 07-VueJs/03-vuejs-숫자야구/NumberBaseball.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+    <h1>{{result}}</h1>
+    <form v-on:submit="onSubmitform">
+    <input ref="answer" maxlength="4" v-model="value">
+    <button>입력</button>
+    </form>
+    <div>시도 : {{count}}</div>
+</div>
+</template>
+
+<script>
+    export default {
+        data(){
+           return{
+               value : '',
+               result : '',
+               count : 0,
+           }
+        },
+        methods: {
+            onSubmitform(e){
+                e.preventDefault();
+            }
+        },
+    };
+</script>
+
+<style>
+
+</style>
