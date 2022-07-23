15b843797e4d4e6f815b7d353200c57282f1c7b9
bottlesun <bottlesun@gmail.com>
2022-07-24 00:30:27 +0900

    update

--- 11-slackCloneCoding/sleact/bottlesun/components/DMList/index.tsx
+++ 11-slackCloneCoding/sleact/bottlesun/components/DMList/index.tsx
@@ -1,5 +1,4 @@
 import EachDM from '@components/EachDM';
-// import useSocket from '@hooks/useSocket';
 import {CollapseButton} from '@components/DMList/styles';
 import {IDM, IUser, IUserWithOnline} from '@typings/db';
 import fetcher from '@utils/fetcher';
