12bf0debb9e8c6b340092cac11d184ca27e3fc19
bottlesun <bottlesun3514@gmail.com>
2022-08-03 15:49:29 +0900

    animate scroll update

--- 08-kakaoBankGsap/js/main.js
+++ 08-kakaoBankGsap/js/main.js
@@ -9,50 +9,61 @@ tl.from('.oh', {
   ease: 'expo.inout', // 애니메이션 부드러움정도?  https://greensock.com/docs/v3/Eases
   //stagger - 시차
 }, 0).from('.logo-box', { // logo
-   opacity: 0, y: -50, ease: 'expo.inout'}, 1)
-  .from('li:nth-child(3), li:nth-child(4)', {
-    opacity: 0, y: 50, ease: 'expo.inout'}, 1)
+  opacity: 0, y: -50, ease: 'expo.inout'
+}, 0)
   .from('.friends', {
-     opacity: 0, y: 50,ease: 'expo.inout'}, 1)
+    opacity: 0, y: 50, ease: 'expo.inout'
+  }, 1)
 
 
 /* ---------------------------------------------------------------------------- */
 let videosHeight = document.querySelector('.videos').clientHeight
 let dumpHeight = document.querySelector('.dump-section').clientHeight
-const Heights = (dumpHeight - videosHeight) / 2
+
+let mainHeight = document.querySelector('main ').clientHeight
+const Heights = (mainHeight + dumpHeight)
+console.log(mainHeight)
 console.log(Heights)
-console.log(videosHeight)
 
 gsap.timeline({
   scrollTrigger: {
-    start: ".main_wrap",
-    endTrigger:'.main_wrap',
+    trigger: ".main_wrap",
+    endTrigger: '.main_wrap',
+    start: 'top 0%',
+    end: 'bottom 0%',
     scrub: 2,
-
+    pin: true,
   }
 }).from(".oh", {scale: 1,}, 0)
-  .to('li:nth-child(3), li:nth-child(4)', {y: -50, opacity: 0 }, 0)
+  .to('li:nth-child(3), li:nth-child(4)', {y: -50, opacity: 0}, 0)
+  .to('.Surprise', {y: -50, opacity: 1}, ">")
   .to('.ryan', {y: -200, x: -150, scale: 2, opacity: 1}, 0)
   .to('.chun', {y: 320, x: 150, scale: 2, opacity: 1}, 0)
-  .to('.Surprise', {y: -50, opacity: 1}, 1)
-  .to('.main_wrap', {scale: 2, opacity: 0 , display :'none'}, 2)
-  .to('.block', {opacity: 0}, 2)
-  .to('.mask',{opacity:0, scale: 5 },3)
-  .to('.activeBg',{opacity:1},4)
-
-/*
+  .to('.main_wrap', {scale: 2, opacity: 0, display: 'none'}, 2)
+  .to('.block', {opacity: 0}, 3)
+  .to('.mask', {scale: 5}, ">")
+  .to('.mask', {opacity: 0}, ">")
+  .to('.activeBg', {opacity: 1}, ">")
+  .to('.cute_chun', {y: -50, opacity: 1}, "<")
 
 gsap.timeline({
-  scrollTrigger:{
-    start: '.information',
+  scrollTrigger: {
+    trigger: ".videos",
+    start: 'bottom 0% - 200px',
+    endTrigger: '.information',
     scrub: 2,
+    onToggle: (e) => PositionEvent(e),
   }
-}).to('.videos,.logo' , {position:"absolute", top : `${dumpHeight}` })
-*/
+})
 
-/*
-window.addEventListener('scroll', (e) => {
-  let scrollY = this.scrollY;
-  let scrollX = this.scrollX;
-  console.log('x,y' , scrollX , scrollY)
-})*/
+const PositionEvent = (e) => {
+  console.log(e.isActive)
+  const logo = document.querySelector('.logo');
+  const videos = document.querySelector('.videos');
+  const arr = [videos,logo];
+
+  arr.map((i,v) => {
+    (!e.isActive) ? (i.style.cssText = 'top:0; position:fixed') : (i.style.cssText = `top:100%; position:absolute `)
+  })
+}
+
