2b4f071cf0fe73e928f5d72593cd3c278af903f9
bottlesun <bottlesun3514@gmail.com>
2023-01-02 18:52:57 +0900

    Update | Entity Create in user, sub , base

---
+++ 09-redditClone-nextjs/server/src/entities/Sub.ts
@@ -0,0 +1,50 @@
+import {Expose} from "class-transformer";
+import {Column, Entity, Index, ManyToOne, OneToMany} from "typeorm";
+import {JoinColumn} from "typeorm/browser";
+import BaseEntity from "./Entity";
+import {User} from "./User";
+
+@Entity("subs")
+export class Sub extends BaseEntity {
+  @Index()
+  @Column()
+  name: string;
+
+  @Column({type: "text", nullable: true})
+  description: string;
+
+  @Column({nullable: true})
+  imageUrn: string;
+
+  @Column({nullable: true})
+  bannerUrn: string;
+
+  @Column()
+  username: string;
+
+  @ManyToOne(() => User)
+  //JoinColumn 은 외래키를 설정한다.
+  // name 은 외래키를 설정하는 컬럼을 설정한다.
+  // name 의 디폴트 값은 외래키를 참조하는 컬럼의 이름 + Id 이다.
+  // referencedColumnName 는 외래키를 참조하는 컬럼을 설정한다.
+  // referencedColumnName 의 디폴트 값은 id 이다.
+  @JoinColumn({name: "username", referencedColumnName: "username"})
+  user: User;
+
+  @OneToMany(() => Post, post => post.sub)
+  post: Post[];
+
+  // Expose 는 class-transformer 를 사용하여 class 를 json 으로 변환할 때 사용한다.
+  @Expose()
+  get imageUrl(): string {
+    //process.env.APP_URL = http://localhost:4000
+    return this.imageUrn ? `${process.env.APP_URL}/images/${this.imageUrn}` :
+      "https://www.gravatar.com/avatar?d=mp&f=y";
+  }
+
+  @Expose()
+  get bannerUrl(): string | undefined {
+    return this.bannerUrn ? `${process.env.APP_URL}/images/${this.bannerUrn}` : undefined;
+  }
+
+}
